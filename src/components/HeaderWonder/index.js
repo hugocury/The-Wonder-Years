a4c20cef94978c16f1a740882574c602b3c72f0f
unknown <hugocury@gmail.comm>
2020-08-04 23:50:09 -0300

    first commit

---
+++ src/components/HeaderWonder/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const HeaderWonder = styled.header`
+  padding: 12px 30px;
+  background-color: var(--color-black-dark);
+  border-bottom: 3px solid #00CED1;
+`;
+
+export default HeaderWonder;
---
+++ src/components/LogoWonder/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import logo from '../../assets/img/Logo3.png';
+
+const LogoWonder = styled.img.attrs({src: logo , alt: 'Logo The Wonder Years'})`
+  height: 60px;
+`;
+
+export default LogoWonder;
