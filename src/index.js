ba037b44c91669a2127377ebebe386ca43db56e7
unknown <hugocury@gmail.comm>
2020-08-06 21:31:00 -0300

    react router dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './styles/settings/colors.css';
 import './styles/reset.css';
+import Video from './pages/Video/Index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/video/new" component={Video} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Video/Index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Video () {
+  return (
+    <h1>Cool cool cool</h1>
+  );
+}
+
+export default Video;
