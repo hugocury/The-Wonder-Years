6354336e0a4f0da690605eb09caf130d3b8fa6fa
unknown <hugocury@gmail.comm>
2020-08-10 17:13:37 -0300

    button effect

--- src/components/ButtonWonder/index.js
+++ src/components/ButtonWonder/index.js
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
 
 const ButtonWonder = styled.button`
-  text-decoration: nome;
   padding: 12px 24px;
   background-color: var(--color-black);
   color: var(--color-blue);
   border: 1px solid var(--color-blue);  
   font-size: 12rem;
   cursor: pointer;
+  text-decoration: nome;
+  transition: transform 100ms cubic-bezier(0, 0, 0.73, 2.24);
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  &:active {
+    transition: transform 100ms linear; 
+    transform: scale(1);
+  }
 `;
 
 export default ButtonWonder;
